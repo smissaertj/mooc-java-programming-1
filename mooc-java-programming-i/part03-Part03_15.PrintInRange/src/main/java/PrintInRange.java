73c39327c6e323997ad2b78cff229efc328cae08
Joeri Smissaert <smissaertj@gmail.com>
2024-02-18 21:07:31 +0400

    Updated exercises

--- mooc-java-programming-i/part03-Part03_15.PrintInRange/src/main/java/PrintInRange.java
+++ mooc-java-programming-i/part03-Part03_15.PrintInRange/src/main/java/PrintInRange.java
@@ -5,6 +5,22 @@ public class PrintInRange {
 
     public static void main(String[] args) {
         // Try your method here
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        printNumbersInRange(numbers, 1, 5);
+    }
+
+    public static void printNumbersInRange(ArrayList<Integer> numbers, int lowerLimit, int upperLimit) {
+        System.out.println("The numbers in the range ["+ lowerLimit + ", " + upperLimit + "]");
+        for (int number : numbers){
+            if (number >= lowerLimit && number <= upperLimit) {
+                System.out.println(number);
+            }
+        }
     }
     
 }
--- mooc-java-programming-i/part03-Part03_16.Sum/src/main/java/Sum.java
+++ mooc-java-programming-i/part03-Part03_16.Sum/src/main/java/Sum.java
@@ -5,6 +5,21 @@ public class Sum {
 
     public static void main(String[] args) {
         // Try your method here
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+
+        System.out.println(sum(numbers));
+
     }
 
+    public static int sum(ArrayList<Integer> numbers){
+        int sum = 0;
+
+        for (int number : numbers){
+            sum += number;
+        }
+
+        return sum;
+    }
 }
