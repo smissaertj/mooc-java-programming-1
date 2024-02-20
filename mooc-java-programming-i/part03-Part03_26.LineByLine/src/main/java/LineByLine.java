88c991c54aa95a7f996ce3f7ee4f7ae3af220c7f
Joeri Smissaert <smissaertj@gmail.com>
2024-02-20 23:06:57 +0400

    Completed Part03_26

--- mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -6,6 +6,25 @@ public class LineByLine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String input = "";
+
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            if(line.isEmpty()){
+                break;
+            }
+            input += line + " ";
+        }
+
+        if(input.length() == 0 ){
+            System.exit(1);
+        }
+
+        String[] splitInput = input.split(" ");
+        for (int i = 0; i < splitInput.length; i++){
+            System.out.println(splitInput[i]);
+        }
+
 
     }
 }
