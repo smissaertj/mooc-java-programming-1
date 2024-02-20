189f9e309ab00aabb056ea679d9b3093b02253ba
Joeri Smissaert <smissaertj@gmail.com>
2024-02-20 22:01:09 +0400

    Completed Part03_25

--- mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
+++ mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
@@ -4,7 +4,22 @@ import java.util.Scanner;
 public class Login {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); 
+
+        String[] alex = {"alex", "sunshine"};
+        String[] emma = {"emma", "haskell"};
+
+        System.out.println("Enter username:");
+        String userName = scanner.nextLine();
+
+        System.out.println("Enter password:");
+        String password = scanner.nextLine();
+
+        if(userName.equals(alex[0]) && password.equals(alex[1]) || userName.equals(emma[0]) && password.equals(emma[1])){
+            System.out.println("You have successfully logged in!");
+        } else {
+            System.out.println("Incorrect username or password!");
+        }
 
     }
 }
