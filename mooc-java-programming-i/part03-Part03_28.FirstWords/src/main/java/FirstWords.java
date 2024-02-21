672fe6c4299b70fb9891b43f374910cada60ee33
Joeri Smissaert <smissaertj@gmail.com>
2024-02-21 22:23:04 +0400

    Completed new exercises

--- mooc-java-programming-i/part03-Part03_28.FirstWords/src/main/java/FirstWords.java
+++ mooc-java-programming-i/part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -6,6 +6,15 @@ public class FirstWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while(true){
+            String input = scanner.nextLine();
 
+            if(input.equals("")){
+                break;
+            }
+
+            String[] splitInput = input.split(" ");
+            System.out.println(splitInput[0]);
+        }
     }
 }
--- mooc-java-programming-i/part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ mooc-java-programming-i/part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -6,6 +6,15 @@ public class LastWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while(true){
+            String input = scanner.nextLine();
 
+            if(input.equals("")){
+                break;
+            }
+
+            String[] splitInput = input.split(" ");
+            System.out.println(splitInput[splitInput.length - 1]);
+        }
     }
 }
