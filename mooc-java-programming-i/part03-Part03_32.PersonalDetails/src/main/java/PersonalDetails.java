2dfff0e403c9c4b1f50ae4b78dcfe0face928a2f
Joeri Smissaert <smissaertj@gmail.com>
2024-02-21 23:05:03 +0400

    Completed new exercises

--- mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -7,6 +7,43 @@ public class PersonalDetails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<Integer> years = new ArrayList<>();
+
+        while(true){
+            String input = scanner.nextLine();
+
+            if(input.equals("")){
+                break;
+            }
+
+            String[] splitInput = input.split(",");
+            names.add(splitInput[0]);
+            years.add(Integer.valueOf(splitInput[1]));
+        }
+
+        // Calculate the average of the birth years        
+        int sumOfYears = 0;
+        for(int year : years){
+            sumOfYears += year;
+        }
+        
+        double averageOfYears = (double) sumOfYears / years.size();
+
+        // Check for the longest name 
+        String longestName = "";
+        int longestNameLength = 0;
+
+        for(String name : names){
+            if(name.length() > longestNameLength){
+                longestNameLength = name.length();
+                longestName = name;
+            }
+        }
+
+        // Print the output to stdout
+        System.out.println("Longest name: " + longestName);
+        System.out.println("Average of the birth years: " + averageOfYears);
 
     }
 }
