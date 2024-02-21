69c8ef6b66d1241d8c34e80783cfaa606ee84711
Joeri Smissaert <smissaertj@gmail.com>
2024-02-21 22:39:09 +0400

    Completed new exercises

--- mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AgeOfTheOldest {
@@ -6,6 +7,27 @@ public class AgeOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        ArrayList<Integer> ages = new ArrayList<>();
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if(input.equals("")){
+                break;
+            }
+
+            String[] splitInput = input.split(",");
+            ages.add(Integer.valueOf(splitInput[1]));
+        }
+
+        int oldest = 0;
+        for(int i = 0; i < ages.size(); i++){
+            if(ages.get(i) > oldest){
+                oldest = ages.get(i);
+            }
+        }
+
+        System.out.println("Age of the oldest: " + oldest);
 
     }
 }
