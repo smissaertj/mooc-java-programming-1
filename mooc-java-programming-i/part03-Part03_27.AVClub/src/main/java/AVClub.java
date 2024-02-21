08ed5b9044839be11ed7d5d19692e89c8b9d1f39
Joeri Smissaert <smissaertj@gmail.com>
2024-02-21 22:10:59 +0400

    Completed new exercises

--- mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AVClub {
@@ -6,6 +7,30 @@ public class AVClub {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String input = "";
+
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+
+            if(line.isEmpty()){
+                break;
+            }
+
+            input += line + " ";
+        }
+
+        if(input.length() == 0){
+            System.exit(1);
+        }
+
+        String[] splitInput = input.split(" ");
+
+        for (int i = 0 ; i < splitInput.length; i++){
+            if(splitInput[i].contains("av")){
+                System.out.println(splitInput[i]);
+            }
+
+        }
 
     }
 }
