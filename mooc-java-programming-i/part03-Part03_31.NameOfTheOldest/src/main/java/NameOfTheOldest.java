bc9054cec7612e6fea211b8e9868416e0a3ef057
Joeri Smissaert <smissaertj@gmail.com>
2024-02-21 22:49:29 +0400

    Completed new exercises

--- mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -1,11 +1,37 @@
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class NameOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        ArrayList<Integer> ages = new ArrayList<>();
+        ArrayList<String> names = new ArrayList<>();
+
+        while(true){
+            String input = scanner.nextLine();
+
+            if(input.equals("")){
+                break;
+            }
+
+            String[] splitInput = input.split(",");
+            names.add(splitInput[0]);
+            ages.add(Integer.valueOf(splitInput[1]));
+        }
+
+        int oldest = 0;
+        int indexOldest = 0;
+        for (int i = 0; i < ages.size(); i++){
+            if(ages.get(i) > oldest){
+                oldest = ages.get(i);
+                indexOldest = i;
+            }
+        }
+
+        System.out.println("Name of the oldest: " + names.get(indexOldest));
 
     }
 }
