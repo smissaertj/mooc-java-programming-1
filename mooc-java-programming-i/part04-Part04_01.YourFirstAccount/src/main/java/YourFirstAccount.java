b2afb5b71f2b5698345588fc8249459aad91decf
Joeri Smissaert <smissaertj@gmail.com>
2024-02-22 21:36:11 +0400

    Added new exercises

--- mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -2,7 +2,9 @@
 public class YourFirstAccount {
 
     public static void main(String[] args) {
-        // Do not touch the code in Account.java
         // Write your program here
+        Account myAccount = new Account("Joeri", 100.0);
+        myAccount.deposit(20);
+        System.out.println(myAccount);
     }
 }
