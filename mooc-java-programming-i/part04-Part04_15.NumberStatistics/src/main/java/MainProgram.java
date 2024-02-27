fbbc9f3b5f005d13e813b61db9ad57dab9ce952e
Joeri Smissaert <smissaertj@gmail.com>
2024-02-27 21:32:28 +0400

    Completed new exercises

--- mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -5,12 +5,30 @@ public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // you can write test code here
-        // however, remove all unnecessary code when doing the final parts of the exercise
 
-        // In order for the tests to work, the objects must be created in the
-        // correct order in the main program. First the object that tracks the total
-        // sum, secondly the object that tracks the sum of even numbers, 
-        // and lastly the one that tracks the sum of odd numbers!
+        Statistics sumAllNumbers = new Statistics();
+        Statistics sumEvenNumbers = new Statistics();
+        Statistics sumOddNumbers = new Statistics();
+
+        System.out.println("Enter numbers:");
+        while (true){
+            int input = Integer.valueOf(scanner.nextLine());
+
+            if (input == -1){
+                break;
+            }
+
+            if(input % 2 == 0){
+                sumEvenNumbers.addNumber(input);
+            } else {
+                sumOddNumbers.addNumber(input);
+            }
+
+            sumAllNumbers.addNumber(input);
+        }
+
+        System.out.println("Sum:"+ sumAllNumbers.sum());
+        System.out.println("Sum of even numbers:" + sumEvenNumbers.sum());
+        System.out.println("Sum of odd numbers:" + sumOddNumbers.sum());
     }
 }
