aac31aba81d0cabd2b7c268f45ea792044d6595f
Joeri Smissaert <joeri@joerismissaert.dev>
2024-03-16 11:19:11 +0400

    Completed Part 4

--- mooc-java-programming-i/part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
+++ mooc-java-programming-i/part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
@@ -25,7 +25,20 @@ public class StoringRecords {
 
         // Write here the code for reading from file
         // and printing the read records
-        return persons;
+        try (Scanner fileReader = new Scanner(Paths.get(file))){
+            while(fileReader.hasNextLine()){
+                String line = fileReader.nextLine();
 
+                String[] parts = line.split(",");
+                String name = parts[0];
+                int age = Integer.valueOf(parts[1]);
+
+                persons.add(new Person(name, age));
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        
+        return persons;
     }
 }
--- mooc-java-programming-i/part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ mooc-java-programming-i/part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -7,6 +7,45 @@ public class SportStatistics {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("File:");
+        String file = scan.nextLine();
+
+        System.out.println("Team:");
+        String team = scan.nextLine();
+
+        ArrayList<String> games = new ArrayList<>();
+        int winCounter = 0;
+        int lossCounter = 0;
+        
+
+        try(Scanner fileReader = new Scanner(Paths.get(file))){
+
+            while(fileReader.hasNextLine()){
+                String line = fileReader.nextLine();
+
+                String[] parts = line.split(",");
+                String homeTeam = parts[0];
+                String visitingTeam = parts[1];
+                int homePoints = Integer.valueOf(parts[2]);
+                int visitingPoints = Integer.valueOf(parts[3]);
+
+                if(homeTeam.equals(team) || visitingTeam.equals(team)){
+                    games.add(team);
+
+                    if( (homeTeam.equals(team) && homePoints > visitingPoints) || (visitingTeam.equals(team) && visitingPoints > homePoints) ){
+                        winCounter++;
+                    } else {
+                        lossCounter++;
+                    }
+                }
+            }
+
+            System.out.println("Games: " + games.size());
+            System.out.println("Wins: " + winCounter);
+            System.out.println("Losses: " + lossCounter);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
 
     }
 
