8a078ef6443c351b601fc7be45752c957f69f9d4
Joeri Smissaert <joeri@joerismissaert.dev>
2024-03-31 16:18:50 +0300

    Completed Ex. Part05_11

--- mooc-java-programming-i/part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ mooc-java-programming-i/part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -3,12 +3,44 @@ public class Apartment {
 
     private int rooms;
     private int squares;
-    private int princePerSquare;
+    private int pricePerSquare;
 
     public Apartment(int rooms, int squares, int pricePerSquare) {
         this.rooms = rooms;
         this.squares = squares;
-        this.princePerSquare = pricePerSquare;
+        this.pricePerSquare = pricePerSquare;
+    }
+
+    public int getPrice(){
+        return this.squares * this.pricePerSquare;
+    }
+
+    public boolean largerThan(Apartment comparedApt){
+        if (this.rooms > comparedApt.rooms){
+            return true;
+        }
+
+        if (this.squares > comparedApt.squares){
+            return true;
+        }
+
+        return false;
+    }
+
+    public int priceDifference(Apartment comparedApt){
+        if (this.getPrice() > comparedApt.getPrice()){
+            return this.getPrice() - comparedApt.getPrice();
+        } else {
+            return comparedApt.getPrice() - this.getPrice();
+        }
+    }
+
+    public boolean moreExpensiveThan(Apartment comparedApt){
+        if (this.getPrice() > comparedApt.getPrice()){
+            return true;
+        }
+
+        return false;
     }
 
 }
