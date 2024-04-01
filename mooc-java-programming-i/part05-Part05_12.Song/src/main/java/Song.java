245f9ffb734f1563e66854af6e93f5d55dd54fdd
Joeri Smissaert <joeri@joerismissaert.dev>
2024-04-01 15:46:55 +0300

    Completed ex. Part05_12

--- mooc-java-programming-i/part05-Part05_12.Song/src/main/java/Song.java
+++ mooc-java-programming-i/part05-Part05_12.Song/src/main/java/Song.java
@@ -11,6 +11,32 @@ public class Song {
         this.durationInSeconds = durationInSeconds;
     }
 
+    public boolean equals(Object comparedObj){
+
+        // is the memory address the same?
+        if (this == comparedObj){
+            return true;
+        }
+
+        // is the compared object an instance of this class?
+        if (!(comparedObj instanceof Song)){
+            return false; // If not an instance of Song, then the objects can't be equal
+        }
+
+        // convert the object into a Song object for further comparison later on
+        Song comparedSongObj = (Song) comparedObj;
+
+        // Are the values of the objects equal?
+        if (
+            this.artist.equals(comparedSongObj.artist) &&
+            this.name.equals(comparedSongObj.name) &&
+            this.durationInSeconds == comparedSongObj.durationInSeconds
+        ) { return true; }
+
+        // otherwise the objects are not equal
+        return false;
+    }
+    
     @Override
     public String toString() {
         return this.artist + ": " + this.name + " (" + this.durationInSeconds + " s)";
