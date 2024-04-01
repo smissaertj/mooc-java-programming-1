eb0320f7b07067212d6ddaff66c726a02b9930d0
Joeri Smissaert <joeri@joerismissaert.dev>
2024-04-01 16:01:10 +0300

    Completed ex Part05_13

--- mooc-java-programming-i/part05-Part05_13.IdenticalTwins/src/main/java/Person.java
+++ mooc-java-programming-i/part05-Part05_13.IdenticalTwins/src/main/java/Person.java
@@ -15,5 +15,36 @@ public class Person {
         this.weight = weight;
     }
 
-    // implement an equals method here for checking the equality of objects
+    public String getName(){
+        return this.name;
+    }
+
+    public boolean equals(Object comparedObj){
+        // Is the memory address the same?
+        if (this == comparedObj){
+            return true;
+        }
+
+        // is the compared Object an instance of this class?
+        if (!(comparedObj instanceof Person)){
+            return false;
+        }
+
+        // Now that we now that comparedObj is a Person
+        // Convert the compared object of the Object class to a Person object
+        Person comparedPersonObj = (Person) comparedObj;
+
+        // Check if the variables have the same value, if yes then the objects are equal
+        if (
+            (this.name.equals(comparedPersonObj.name)) &&
+            this.height == comparedPersonObj.height &&
+            this.weight == comparedPersonObj.weight &&
+            this.birthday.getDay() == comparedPersonObj.birthday.getDay() &&
+            this.birthday.getMonth() == comparedPersonObj.birthday.getMonth() &&
+            this.birthday.getYear() == comparedPersonObj.birthday.getYear()
+        ){ return true; }
+
+        // Otherwise, objects are not equal
+        return false;
+    }
 }
