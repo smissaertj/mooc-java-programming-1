905adab1120c756b5a81fa0112909c788bc3cf8b
Joeri Smissaert <smissaertj@gmail.com>
2024-04-03 17:22:54 +0300

    Completed Part05_14

--- mooc-java-programming-i/part05-Part05_14.Books/src/main/java/Book.java
+++ mooc-java-programming-i/part05-Part05_14.Books/src/main/java/Book.java
@@ -17,4 +17,26 @@ public class Book {
         return publicationYear;
     }
 
+    public boolean equals(Object compared){
+        // If the objects are located on the same memory address, then they are equal.
+        if (this == compared){
+            return true;
+        }
+
+        // If the compared object is not an instance of this class, they can't be equal.
+        if(!(compared instanceof Book)){
+            return false;
+        }
+
+        // The compard object is an instance of this class, we can now compare the instance variables
+        Book comparedBookObj = (Book) compared;
+        if(
+            this.name.equals(comparedBookObj.name) &&
+            this.publicationYear == comparedBookObj.publicationYear
+        ){return true;}
+
+        // else, the objects are not equal
+        return false;
+    }
+
 }
