7903cee556098b058e0f8d4e623565772b802ecb
Joeri Smissaert <smissaertj@gmail.com>
2024-04-04 17:34:33 +0300

    Completed Part06_01

--- mooc-java-programming-i/part06-Part06_01.Menu/src/main/java/Menu.java
+++ mooc-java-programming-i/part06-Part06_01.Menu/src/main/java/Menu.java
@@ -9,5 +9,29 @@ public class Menu {
         this.meals = new ArrayList<>();
     }
 
-    // implement the required methods here
+    public void addMeal(String meal){
+        if (!(this.meals.contains(meal))){
+            this.meals.add(meal);
+        }
+    }
+
+    public void removeMeal(String meal){
+        if (this.meals.contains(meal)){
+            this.meals.remove(meal);
+        }
+    }
+
+    public void printMeals(){
+        if (this.meals.size() > 0){
+           for (String meal: meals){
+            System.out.println(meal);
+           } 
+        }
+    }
+
+    public void clearMenu(){
+        if (this.meals.size() > 0){
+            this.meals.clear();
+        }
+    }
 }
