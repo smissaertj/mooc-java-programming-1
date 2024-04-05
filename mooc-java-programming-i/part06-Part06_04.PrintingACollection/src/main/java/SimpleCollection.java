0381abecc5df7817f7f580d4172c20dd5bc2c82d
Joeri Smissaert <joeri@joerismissaert.dev>
2024-04-05 14:56:06 +0300

    Completed Part06_04

--- mooc-java-programming-i/part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
+++ mooc-java-programming-i/part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
@@ -18,5 +18,22 @@ public class SimpleCollection {
     public ArrayList<String> getElements() {
         return this.elements;
     }
+
+    @Override
+    public String toString(){
+        String strElements = "";
+        for (String el: elements){
+            strElements = strElements + "\n" + el;
+        }
+
+        if (this.elements.isEmpty()){
+            return "The collection " + this.name + " is empty.";
+        } else if (this.elements.size() == 1){
+            return "The collection " + this.name + " has " + this.elements.size() + " element:" + strElements;
+        } else {
+            return "The collection " + this.name + " has " + this.elements.size() + " elements:" + strElements;
+        }
+
+    }
     
 }
