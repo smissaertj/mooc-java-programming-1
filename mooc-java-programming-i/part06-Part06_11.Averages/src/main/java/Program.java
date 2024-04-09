fcffa8f73b8a06377844a7a339933ed1367183c9
Joeri Smissaert <smissaertj@gmail.com>
2024-04-09 15:53:30 +0300

    Completed Part06_11

--- mooc-java-programming-i/part06-Part06_11.Averages/src/main/java/Program.java
+++ mooc-java-programming-i/part06-Part06_11.Averages/src/main/java/Program.java
@@ -5,9 +5,7 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         GradeRegister register = new GradeRegister();
-
         UserInterface ui = new UserInterface(register, scanner);
         ui.start();
     }
