2522c635ed9da502840963095a9e86bf079bf904
Joeri Smissaert <smissaertj@gmail.com>
2024-04-11 23:07:07 +0300

    Completed Part07_04, Part07_05

--- mooc-java-programming-i/part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
+++ mooc-java-programming-i/part07-Part07_04.ReadymadeSortingAlgorithms/src/main/java/Main.java
@@ -1,10 +1,50 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-        // insert test code here
         int[] array = {3, 1, 5, 99, 3, 12};
+        String[] strArray = {"Omega", "Beta", "Alpha"};
+        ArrayList<Integer> listArrInt = new ArrayList<>();
+        ArrayList<String> listArrStr = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++){
+            Random random = new Random();
+            int randomInt = random.nextInt(11);
+            listArrInt.add(randomInt);
+        }
+
+        for (String str: strArray){
+            listArrStr.add(str);
+        }
+
+        sort(array);
+        sort(strArray);
+        sortIntegers(listArrInt);
+        sortStrings(listArrStr);
+    }
+
+    public static void sort(int[] array ){
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void sort(String[] array){
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void sortIntegers(ArrayList<Integer> integers){
+        Collections.sort(integers);
+        System.out.println(integers);
+    }
+
+    public static void sortStrings(ArrayList<String> strings){
+        Collections.sort(strings);
+        System.out.println(strings);
     }
 
 }
--- mooc-java-programming-i/part07-Part07_05.Searching/src/main/java/Searching.java
+++ mooc-java-programming-i/part07-Part07_05.Searching/src/main/java/Searching.java
@@ -44,10 +44,34 @@ public class Searching {
     }
 
     public static int linearSearch(ArrayList<Book> books, int searchedId) {
+        for (Book book: books){
+            if (book.getId() == searchedId){
+                return books.indexOf(book);
+            }
+        }
         return -1;
     }
 
     public static int binarySearch(ArrayList<Book> books, long searchedId) {
+
+        int start = 0;
+        int end = books.size() - 1;
+
+        while (start <= end ){
+            int middle = (start + end) / 2;
+
+            if (books.get(middle).getId() == searchedId){
+                return middle;
+            }
+            
+            if (books.get(middle).getId() < searchedId){
+                start =  middle + 1;
+            }
+
+            if (books.get(middle).getId() > searchedId){
+                end = middle - 1;
+            }
+        }
         return -1;
     }
 }
